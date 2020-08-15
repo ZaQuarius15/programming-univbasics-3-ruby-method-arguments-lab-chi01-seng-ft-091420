b3f8fda091c8551882e6e7c5ad3bf61ed30908cc
Zack Carlson <jzcarlson014@gmail.com>
2020-08-15 19:02:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -18,9 +18,9 @@ def introduction_with_language_optional(name, language=Ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction(Zack)
-introduction_with_language(Zack,Ruby)
-introduction_with_language_optional(Zack)
+introduction("Zack")
+introduction_with_language("Zack","Ruby")
+introduction_with_language_optional("Zack")
 
 
 
